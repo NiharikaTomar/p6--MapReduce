89defa84fd87fe3a0ab144fcf51cad48453b149f
Elizaveta Stepanova <elizaveta@royal-03.cs.wisc.edu>
2019-11-16 18:49:51 -0600

    Emit + wrapper attempt 1

--- mapreduce.c
+++ mapreduce.c
@@ -7,58 +7,103 @@
 #include <semaphore.h>
 #include "mapreduce.h"
 
-struct pair {
+// Locks
+pthread_mutex_t f_lock;
+pthread_mutex_t lock;
+
+struct map_keyvals {
     char *key;
     char *value;
-    struct pair *next;
-    int proc_thread = 0; // to keep track if processed
+    // struct pair *next;
+    // int proc_thread = 0; // to keep track if processed
 };
 
-struct reduce_stuff {
-    char *key;
-    Getter get_func;
-    int partition_number;
+struct files {
+    char *file_name;
 };
 
-struct partition_stuff {
-    struct pair *head;
+struct partition {
+    int *processed_times;
+    int *mapped_p;
+    int *mapped_keyvals_amount;
 };
 
-Partioner p;
+struct files *file_array;
+struct map_keyvals **partition_array;
+
 int NUM_PART;
+int total_files;
+int amt_files;
 
-partition_stuff *partition_array;
-
+Partitioner part;
+Mapper mapp;
+Reducer red;
 
 // Gets the next value that will be used in user Reduce
 char* get_next(char *key, int partition_number){
-    struct pair *current_pair;
-    current_pair = p[partition_number].head;
-
-    while (current_pair != NULL) {
-        if (current_pair->proc_thread == 0) {
-            // Return the value with the key found
-            if (current_pair->key == key) {
-                if (current_pair->next != NULL) {                
-                    // set it to processed
-                    current_pair->proc_thread = 1;
-                    return current_pair->next->value;
-                }
-            }
+    if (partition->mapped_keyvals_amount >= partition->processed_times) {
+        if (strcmp(partition_array[partition_number][partition->processed_times], key) == 0) {
+            partition->processed_times = partition->processed_times + 1;
+            return partition_array[partition_number][partition->processed_times].value;
         }
-        current_pair = current_pair->next;
     }
     return NULL;
 }
 
 void MR_Emit(char *key, char *value){
-    
+    pthread_mutex_lock(&lock);
+
+    // Partition number based on user's choice: MR_SortedPartition, 
+    // MR_DefaultHashPartition, or anything else user provides.
+    unsigned long p_num = part(key, NUM_PART);
+
+    partition->mapped_keyvals_amount[p_num] = partition->mapped_keyvals_amount[p_num] + 1;
+
+    // Check if reallocation is needed
+    if (partition->mapped_p[p_num] < partition->mapped_keyvals_amount[p_num]) {
+        partition->mapped_p[p_num] = partition->mapped_p[p_num] * 2; // Double the size of the hash table
+        partition_array[p_num] = realloc(partition_array[p_num], partition->mapped_p[p_num] * sizeof(struct map_keyvals)); // Reallocating the hash table size
+    }
+
+    // Allocating space in the hash table for key value pair
+    partition_array[p_num][partition->mapped_keyvals_amount[p_num] - 1].key = (char *) malloc(strlen(key) + 1 * 1);
+    partition_array[p_num][partition->mapped_keyvals_amount[p_num] - 1].value = (char *) malloc(strlen(value) + 1 * 1);
+
+    // Putting key and value pair in the hash table
+    strcpy(partition_array[p_num][partition->mapped_keyvals_amount[p_num] - 1].key, key);
+    strcpy(partition_array[p_num][partition->mapped_keyvals_amount[p_num] - 1].value, value);
+
+    pthread_mutex_unlock(&lock);
 }
 
+// Wrapper for Mapper
+void* mapper_run(void *start) {
+    for (int i = amt_files; i < total_files; i++) {
+        pthread_mutex_lock(&f_lock);
 
-// void reducer_run(*partition) {
-//
-// }
+        char *cf = file_array[amt_files].file_name;
+
+        amt_files = amt_files + 1;
+
+        pthread_mutex_unlock(&f_lock);
+        // Call mapper on current file
+        mapp(cf);
+    }
+    return start;
+}
+
+// Wrapper for Reducer
+void* reducer_run(void *start) {
+    int i = 0;
+    while (i < partition->mapped_keyvals_amount[*(int *) start]) {
+        if (partition->processed_times[*(int *) start] == i) {
+            // Call reducer on the partitions
+            red(partition_array[*(int *) start][i].key, get_next, *(int *) start);
+        }
+        i++;
+    }
+    return start;
+}
 
 void MR_Run(int argc, char *argv[], 
         Mapper map, int num_mappers, 
@@ -69,42 +114,49 @@ void MR_Run(int argc, char *argv[],
     if (argc - 1 < num_mappers) {
         num_mappers = argc - 1;
     }
+
+    struct partition *p;
     
+    p->mapped_keyvals_amount = malloc(num_partitions * 4);
+    p->processed_times = malloc(num_partitions * 4);
+    p->mapped_p = malloc(num_partitions * 4);
+
+    partition_array = malloc(num_partitions * sizeof(struct map_keyvals));
+    
+    part = partition;
+    mapp = map;
+    red = reduce;
+    NUM_PART = num_partitions;
+    total_files = argc - 1;
+    amt_files = 0;
+
     pthread_t mapper_thread[num_mappers];
     pthread_t reducer_thread[num_reducers];
-    p = partition;
-    NUM_PART = num_partitions;
     
-    partition_array = malloc(num_partitions * sizeof(partition_stuff));
 
-    for (int i = 0; i < num_partitions; i++) {
-        partition_array[i].head = NULL;
-    }
 
     // To create mapper threads
     for (int i = 0; i < num_mappers; i++) {
-        pthread_create(&mapper_thread[i], NULL, map, argv[i]);
+        pthread_create(&mapper_thread[i], NULL, mapper_run, NULL);
     }
     // To join mapper threads
     for (int i = 0; i < num_mappers; i++) {
-        pthread_join(&mapper_thread[i], NULL);
+        pthread_join(mapper_thread[i], NULL);
     }
 
     // MERGE SORT or qsort or any sort with nlogn complexity
 
-    // struct reduce_info;
-
     // To create reducer threads
     for (int i = 0; i < num_reducers; i++) {
-        pthread_create(&reducer_thread[i], NULL, reduce, NULL);
-        // pthread_create(&reducer_thread[i], NULL, reducer_run, reduce_info);
+        pthread_create(&reducer_thread[i], NULL, reducer_run, NULL);
     }
     // To join reducer threads
     for (int i = 0; i < num_reducers; i++) {
-        pthread_join(&reducer_thread[i], NULL);
+        pthread_join(reducer_thread[i], NULL);
     }
 
-    // freeee me !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Free dinamically allocated memory
+    // free(partition_array);
 }
 
 unsigned long MR_SortedPartition(char *key, int num_partitions) {
